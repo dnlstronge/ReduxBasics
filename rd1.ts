212c7f0e9746d11c4f2c4df51bf375c6107e0b60
Dan Stronge <dnlstronge@gmail.com>
2023-05-12 13:17:35 +0100

    [x] - add logic

--- rd1.ts
+++ rd1.ts
@@ -2,10 +2,12 @@ const redux = require("redux")
 
 
 const counterReducer = (state, action) =>  {
-
+    return {
+        counter: state.counter + 1
+    }
 }
 
-const store = redux.createStore()
+const store = redux.createStore(counterReducer)
 
 
 // the reducer function will produce new state snapshots
