fef3bffac41638b78eaec5b35434d9f4bb9b7f1a
Dan Stronge <dnlstronge@gmail.com>
2023-05-12 13:41:14 +0100

    [x] - add case to reducer

--- rd1.ts
+++ rd1.ts
@@ -2,9 +2,12 @@ const redux = require("redux")
 
 
 const counterReducer = (state = {counter: 0}, action) =>  {
-    return {
-        counter: state.counter + 1
+    if(action.type === "INCREMENT") {
+        return {
+            counter: state.counter + 1
+        }
     }
+    
 }
 
 
